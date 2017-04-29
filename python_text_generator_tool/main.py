9e6f872868d25220bc51ef7c85fdfe57ed7ff5bd
jakejaga <jake-basu@hotmail.co.jp>
2017-04-29 20:11:04 +0900

    fixed nested parenthesis and quotes bug

--- python_text_generator_tool/main.py
+++ python_text_generator_tool/main.py
@@ -11,6 +11,8 @@ def split_expr(s):
     el = ""
     eval_mode = 0
     level = 0
+    prev_escape = 0
+    in_string = None
     for c in s:
         if eval_mode == 0:
             if c == '#': break
@@ -23,19 +25,25 @@ def split_expr(s):
 
             if eval_mode != 0: level = 1
         else:
-            if eval_mode == 1:
+            if eval_mode in [1, 8, 16, 32]:
+                if c in '\'"':
+                    if in_string is None:
+                        in_string = c
+                    elif not prev_escape and in_string == c:
+                        in_string = None
+            if eval_mode == 1 and not in_string:
                 if c == '`': level = 0
-            if eval_mode == 2:
+            if eval_mode == 2 and not prev_escape:
                 if c == '"': level = 0
-            if eval_mode == 4:
+            if eval_mode == 4 and not prev_escape:
                 if c == "'": level = 0
-            if eval_mode == 8:
+            if eval_mode == 8 and not in_string:
                 if c == '[': level += 1
                 if c == ']': level -= 1
-            if eval_mode == 16:
+            if eval_mode == 16 and not in_string:
                 if c == '(': level += 1
                 if c == ')': level -= 1
-            if eval_mode == 32:
+            if eval_mode == 32 and not in_string:
                 if c == '{': level += 1
                 if c == '}': level -= 1
 
@@ -46,6 +54,7 @@ def split_expr(s):
             sps.append(c)
         elif eval_mode not in [0, 1] or c != '`':
             el += c
+        prev_escape = (c == '\\')
     if len(el) > 0:
         sps.append(el)
     return sps
